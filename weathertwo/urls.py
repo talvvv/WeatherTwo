4fa718755d244b2f2133eb316a514fd48a383573
talvvv <kamzatalant@gmail.com>
2022-05-15 09:57:16 +0600

    init

--- weathertwo/urls.py
+++ weathertwo/urls.py
@@ -3,4 +3,5 @@ from weathertwo import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("weathertwo/<name>", views.basic_print, name="basic_print"),
 ]
--- weathertwo/views.py
+++ weathertwo/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def basic_print(request, location):
+    location = input("Enter the city:")
+    unit = "metric"
+    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=2ee40ecb425f72c75ed3612e15cbd107&units=" + unit
+    
+    
